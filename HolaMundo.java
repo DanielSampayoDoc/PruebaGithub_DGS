9d099022906d65ad941a97f30fb233266bf22c4f
PRUEBAUD4 DANIEL BORRAJO FERNÁNDEZ <daniel@pruebaUD4>
2026-04-16 17:49:28 +0200

    Nuevo

--- HolaMundo.java
+++ HolaMundo.java
@@ -2,5 +2,8 @@ public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("¡Hola, Dani!");
         System.out.println("¡Hola, aaron!");
+        System.out.println("¡Hola, Sofia!");
+        System.out.println("¡Hola, Hugo!");
+        System.out.println("¡Hola, Reinaldo!");
     }
 }
